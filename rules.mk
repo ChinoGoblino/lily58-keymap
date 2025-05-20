a87a4be328659289ba9f3994b4d1a812a0de0845
ChinoGoblino <general@ethonium.com>
2025-05-21 02:02:59 +1000

    feat: rearrange lower and raised layers

--- rules.mk
+++ rules.mk
@@ -1,7 +1,7 @@
-OLED_ENABLE = yes
 MOUSEKEY_ENABLE = yes
 EXTRAKEY_ENABLE = yes
 
+OLED_ENABLE = yes
 
 # If you want to change the display of OLED, you need to change here
 #SRC +=  ./lib/rgb_state_reader.c \
